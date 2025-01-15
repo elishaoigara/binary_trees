f698c41f4363829f6ed7bf2dbf76df05161ad3a3
Elisha Oigara <elishaoigara50@gmail.com>
2025-01-15 05:22:32 -0800

    task31

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,15 +1,18 @@
 #include "binary_trees.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /* Helper function to check the height of the tree */
 int height(const binary_tree_t *node)
 {
+    int left_height, right_height;
+
     if (!node)
         return -1;
 
-    int left_height = height(node->left);
-    int right_height = height(node->right);
+    left_height = height(node->left);
+    right_height = height(node->right);
 
     return (left_height > right_height ? left_height : right_height) + 1;
 }
@@ -29,6 +32,8 @@ int is_bst(const binary_tree_t *node, int min, int max)
 /* Main function to check if the tree is a valid AVL Tree */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
+    int left_height, right_height;
+
     if (!tree)
         return 0;
 
@@ -37,8 +42,8 @@ int binary_tree_is_avl(const binary_tree_t *tree)
         return 0;
 
     /* Check if the balance factor is valid */
-    int left_height = height(tree->left);
-    int right_height = height(tree->right);
+    left_height = height(tree->left);
+    right_height = height(tree->right);
 
     if (abs(left_height - right_height) > 1)
         return 0;
