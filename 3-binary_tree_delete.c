a9e0e996fd643e784e71de40c574c07a0e75dc0f
Elisha Oigara <elishaoigara50@gmail.com>
2025-01-15 02:14:45 -0800

    task3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -12,11 +12,11 @@ void binary_tree_delete(binary_tree_t *tree)
     if (tree == NULL)
         return;
 
-    // Recursively delete the left and right children
+    /* Recursively delete the left and right children */
     binary_tree_delete(tree->left);
     binary_tree_delete(tree->right);
 
-    // Free the current node
+    /* Free the current node */
     free(tree);
 }
 
